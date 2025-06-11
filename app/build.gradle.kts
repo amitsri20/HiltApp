09bdf812265a2a3d65b704bf5211af2305501a16
amit srivastava <amitsrivastava200892@gmail.com>
2025-06-11 09:22:35 +0530

    Added Viewmodel test cases

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,10 @@ dependencies {
     implementation(libs.gson)
 
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.turbine)
+    testImplementation(libs.kotlinx.coroutines.test)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
